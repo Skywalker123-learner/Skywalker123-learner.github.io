c7e96933b9892b612a5fcb7ce8673279c1f08c1e
Skywalker123-learner <liaolishen1@gmail.com>
2026-03-26 15:42:35 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
+const CACHE_NAME = 'pomodoro-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
